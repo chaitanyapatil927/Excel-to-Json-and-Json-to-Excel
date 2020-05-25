aa473d068e6cf882739241b6756868cb29aeb564
Chaitanya Patil <chaitanya.patil@sakec.ac.in>
2020-05-25 18:14:00 +0530

    excel to json not proper and unhandeled promise

--- bin/routes/diamonds.js
+++ bin/routes/diamonds.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-var json2xls = require('json2xls');
+var jsonexport = require('jsonexport');
 const fs = require("fs");//npm install fs
 var exceltojson = require("xlsx-to-json-lc");
 
@@ -16,41 +16,18 @@ router.get('/exportfromdb',(req, res, next) =>{
     .exec()
     .then(doc =>{ 
         coolnew = doc;
+        
         console.log(coolnew);
         res.status(200).json(doc);
-        //checking
-        //var rawFile = fs.coolnew//dir of your json file as param
-        //var raw = JSON.parse(coolnew)
-        //console.log(raw);
-        //res.status(200).json(raw);
-        var xlsx = json2xls(coolnew,{
-            fields : {
-                styleNumber:'Number',
-                images:{
-                    main:'string',
-                    white:'string',
-                    yellow:'string',
-                    rose:'string'
-                },
-                diamondWeight:'Number',
-                diamondCount:'Number',
-                goldWeight:'Number',
-                designDetails:{
-                    new:Boolean,
-                    featured:Boolean,
-                    highestSelling:Boolean,
-                    fancyDiamond:Boolean
-                },
-                company:'string',
-                favouriteCount:'Number'}
-         }
-            
-            //,{
-            //fields : {_id:'string',title:'string',body:'string',code:'string',author:'string'}
-       // }
-        );
-        fs.writeFileSync('datain.xlsx', xlsx, 'binary');
-        res.download('datain.xlsx');
+        //unhandeled promise error below this
+        //expected excel file is datain.xls which i got by running the below code seperately
+        jsonexport(coolnew, {rowDelimiter: '\t'}, function(err, csv){
+            if(err) return console.log(err);
+            console.log(csv);
+            fs.writeFileSync('data.xls', csv, 'binary');
+            })
+        
+       
     })
     .catch(err =>{
         console.log(error);
@@ -76,6 +53,8 @@ router.post('/importtodb',(req, res, next) =>{
           console.log(result);
           //result will contain the overted json data
         }
+        //excel to json not working properly for nested values
+        // datas.json is the required format and out.json is the format we are getting with this module 
         Diamond.collection.insert(result,function(err,docs){
             if (err){
                 return console.error(err),
